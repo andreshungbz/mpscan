987cfe711546359db42dd76ff779a16d38a9a318
Andres Hung <2018118240@ub.edu.bz>
2025-03-29 23:49:39 -0600

    Create custom types for managing scans

---
+++ internal/scan/scan.go
@@ -0,0 +1,33 @@
+package scan
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+type Summary struct {
+	Hostname          string
+	TotalPortsScanned int
+	OpenPortCount     int
+	OpenPorts         []int
+	TimeTaken         time.Duration
+}
+
+func (s Summary) String() string {
+	return fmt.Sprintf(
+		"[summary/%s]\nTotal Ports Scanned: %d\nOpen Ports Count: %d\nOpen Ports: %v\nTime Taken: %s",
+		s.Hostname, s.TotalPortsScanned, s.OpenPortCount, s.OpenPorts, s.TimeTaken,
+	)
+}
+
+func (s *Summary) AddPort(port int) {
+	s.OpenPorts = append(s.OpenPorts, port)
+	sort.Ints(s.OpenPorts)
+	s.OpenPortCount = len(s.OpenPorts)
+}
+
+type Address struct {
+	Hostname string
+	Port     int
+}
