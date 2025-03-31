c07f1b99a3ba8be4868843e472afc7fbc0c820fe
Andres Hung <2018118240@ub.edu.bz>
2025-03-30 22:46:44 -0600

    Refactor into local functions and add comments

--- main.go
+++ main.go
@@ -11,47 +11,38 @@ import (
 	"github.com/vbauerster/mpb/v8"
 )
 
-var (
-	targetF    = flag.String("target", "", "the hostname or IP address to be scanned")
-	startPortF = flag.Int("start-port", 1, "the lower bound port to begin scanning")
-	endPortF   = flag.Int("end-port", 1024, "the upper bound port to finish scanning")
-	workersF   = flag.Int("workers", 100, "the number of concurrent goroutines to launch")
-	timeoutF   = flag.Int("timeout", 5, "the maximum time in seconds to wait for a connection to be established")
-)
-
 func main() {
-	var portsF scan.PortList
-	flag.Var(&portsF, "ports", "comma-separated list of ports (e.g., -ports=22,80,443)")
 
-	var targetsF scan.TargetList
-	flag.Var(&targetsF, "targets", "comma-separated list of targets (e.g., -targets=example.com,scanme.nmap.org)")
+	// FLAG VARIABLES SETUP
+
+	var (
+		targetF    = flag.String("target", "", "the hostname or IP address to be scanned")
+		startPortF = flag.Int("start-port", 1, "the lower bound port to begin scanning")
+		endPortF   = flag.Int("end-port", 1024, "the upper bound port to finish scanning")
+		workersF   = flag.Int("workers", 100, "the number of concurrent goroutines to launch")
+		timeoutF   = flag.Int("timeout", 5, "the maximum time in seconds to wait for a connection to be established")
+
+		portsF   scan.PortList
+		targetsF scan.TargetList
+	)
+
+	flag.Var(&portsF, "ports", "comma-separated list of ports (e.g., -ports=22,80,443)")
+	flag.Var(&targetsF, "targets", "comma-separated list of targets (e.g., -targets=localhost,scanme.nmap.org)")
 
 	flag.Parse()
-	fmt.Printf("%v %v %v %v %v %v %v\n", *targetF, *startPortF, *endPortF, *workersF, *timeoutF, portsF, targetsF)
 
-	var targets []string
+	// fmt.Printf("%v %v %v %v %v %v %v\n", *targetF, *startPortF, *endPortF, *workersF, *timeoutF, portsF, targetsF) // values check
 
-	if *targetF != "" {
-		targets = append(targets, *targetF)
-	}
+	// SYNCHRONIZATION SETUP & SCANS
 
-	for _, hostname := range targetsF {
-		targets = append(targets, hostname)
-	}
+	var wg sync.WaitGroup
+	p := mpb.New(mpb.WithWaitGroup(&wg)) // multiple progress bars setup
 
-	if len(targets) == 0 {
-		targets = append(targets, "localhost")
-	}
+	targets := createTargets(*targetF, targetsF)
+	var summaries []scan.Summary
 
 	fmt.Printf("\n[SCAN START]\n\n")
-
-	var summaries []scan.Summary
-	var wg sync.WaitGroup
-	p := mpb.New(mpb.WithWaitGroup(&wg))
-
 	for _, target := range targets {
-		wg.Add(1)
-
 		flags := scan.Flags{
 			Target:    target,
 			StartPort: *startPortF,
@@ -61,6 +52,7 @@ func main() {
 			Ports:     portsF,
 		}
 
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			summaries = append(summaries, connection.CreateSummary(flags, p))
@@ -70,9 +62,35 @@ func main() {
 	wg.Wait()
 	p.Wait()
 
+	// RESULTS
+
+	printResults(summaries, *timeoutF)
+}
+
+// createTargets aggregates all hostnames from the -target and -targets flags.
+//
+// If there are no viable values, a default "localhost" is added.
+func createTargets(target string, targets []string) []string {
+	var results []string
+
+	if target != "" {
+		results = append(results, target)
+	}
+
+	results = append(results, targets...)
+
+	if len(results) == 0 {
+		results = append(results, "localhost")
+	}
+
+	return results
+}
+
+// printResults prints the banners and summaries from the scan.
+func printResults(summaries []scan.Summary, timeout int) {
 	fmt.Printf("\n[BANNERS]\n\n")
 	for _, summary := range summaries {
-		connection.PrintBanner(summary, *timeoutF)
+		connection.PrintBanner(summary, timeout)
 	}
 
 	fmt.Printf("\n[SCAN SUMMARY]\n\n")
